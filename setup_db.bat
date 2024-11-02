4ee9dfde62badd6f203ca82b614d8d5c75496169
Emmanuel <53409725+Ghost49-del@users.noreply.github.com>
2024-11-02 21:20:42 +0200

    Create setup_db.bat

---
+++ setup_db.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Setting up database for the web app...
+
+REM Replace these with your MySQL credentials
+set MYSQL_USER=root
+set MYSQL_PASSWORD=""
+
+REM Path to your MySQL installation
+set MYSQL_PATH="C:\wamp64\bin\mysql\mysql8.3.0\bin\mysql"
+
+REM Run the SQL script
+%MYSQL_PATH% -u %MYSQL_USER% -p%MYSQL_PASSWORD% < setup_database.sql
+
+echo Database setup complete!
+pause
